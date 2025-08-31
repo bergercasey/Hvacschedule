f43ecfcf6baacdee9c540444cb0313bed6e7b8c2
bergercasey <caseyb@advanced-mechanical.com>
2025-08-31 10:47:12 -0500

    Update ping.js

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,12 +1,5 @@
-const { checkAuth } = require('./_auth');
-
-exports.handler = async (event) => {
-  const authErr = checkAuth(event);
-  if (authErr) return authErr;
-
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ok: true })
-  };
-};
+exports.handler = async () => ({
+  statusCode: 200,
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ ok: true })
+});
