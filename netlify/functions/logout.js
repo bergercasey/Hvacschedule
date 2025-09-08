803014db71a5b5c7542c48116b861bd987463f50
bergercasey <caseyb@advanced-mechanical.com>
2025-09-08 17:47:15 -0500

    Delete netlify/functions directory

--- netlify/functions/logout.js
+++
@@ -1,9 +0,0 @@
-const { clearCookie } = require('./_authutil');
-
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { 'Set-Cookie': clearCookie(), 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ok:true })
-  };
-};
