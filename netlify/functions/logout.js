d8d30acc7f6240a564dedf6ffc8834a641d7db52
bergercasey <caseyb@advanced-mechanical.com>
2025-09-08 19:46:26 -0500

    Update logout.js

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,6 +1,9 @@
-const { makeCookie } = require('./_authUtil');
+const { clearCookie } = require('./_authUtil');
+
 exports.handler = async () => {
-  // Expire cookie immediately
-  const gone = makeCookie('x', { remember:false }).replace(/Max-Age=\d+/, 'Max-Age=0');
-  return { statusCode:200, headers:{ 'Set-Cookie': gone, 'Content-Type':'application/json' }, body: JSON.stringify({ ok:true }) };
+  return {
+    statusCode: 200,
+    headers: { 'Set-Cookie': clearCookie(), 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ok:true })
+  };
 };
